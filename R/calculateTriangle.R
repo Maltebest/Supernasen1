5cab37013ff284eb3d0e51ec42d4e647513685ef
unknown <Staeblei@pcrzp861.AD.Uni-Marburg.DE>
2014-06-04 12:59:42 +0200

    calculateTriang

--- R/calculateTriangle.R
+++
@@ -1,6 +0,0 @@
-calculateTriangle <- function(alpha, c) {
-  a <- sin(degToRad(alpha)) * c
-  b <- cos(degToRad(alpha)) * c
-  result <- data.frame(x = round(a, 2), y = round(b, 2))
-  return(result)
-}
